ad2e587eb30512b94abe155bf55658b42334dff2
MaryAnzh <maryang@mail.ru>
2023-01-31 23:54:41 +0300

    feat: add styled-components

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import Link from "next/link";
 import Nav from "@/components/smart/nav/nav";
+import { HeaderStyled } from "./styled";
 
 type HeaderProps = {}
 
 export default function Header(props: HeaderProps) {
     const navList = ['about', 'contacts', 'auth'];
     return (
-        <header>
-            <Nav navList={navList} />
-        </header>
+        <HeaderStyled>
+            <header>
+                <Nav navList={navList} />
+            </header>
+        </HeaderStyled>
+
     );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/common/header/header'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+
   return (
     <Html lang="en">
       <Head />
