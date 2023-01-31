27fb33ef2a0df99d5f043452d23dfb0274d84525
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-01-31 21:13:03 +0300

    add: authorization page

---
+++ src/pages/authorization/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import styles from "./Authorization.module.scss";
+
+const Authorization: FC = () => {
+  return <form className={styles.form}>Form Auth</form>;
+};
+
+export default Authorization;
