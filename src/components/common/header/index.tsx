6b3f34c7f22338b1fb3946aae1cf0fdc6df4baff
MaryAnzh <maryang@mail.ru>
2023-02-01 17:16:35 +0300

    feat: add style

---
+++ src/components/common/header/index.tsx
@@ -0,0 +1,14 @@
+import { Form, Input, Title, Button } from "./styled";
+
+const main_blu_color = 'blue';
+
+const MyHeader = () => {
+
+    return (
+        <div>
+            <Title>MyHeader</Title>
+        </div>
+    );
+};
+
+export default MyHeader;
