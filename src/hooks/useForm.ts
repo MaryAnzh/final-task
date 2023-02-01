2a2dea14c6c7266eb2a9783c7057215fcaabe626
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-02-01 15:41:56 +0300

    add: userApi

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export function useForm<T = { [name: string]: string }>(inputValues: T) {
+  const [values, setValues] = useState(inputValues);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = event.target;
+    setValues({ ...values, [name]: value });
+  };
+  return { values, handleChange, setValues };
+}
