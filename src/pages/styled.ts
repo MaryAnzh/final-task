18e4d02ba0d6cdd186846f184b63783cd437d714
MaryAnzh <maryang@mail.ru>
2023-02-15 13:47:02 +0300

    feat: new path for about bg img

--- src/pages/styled.ts
+++ src/pages/styled.ts
@@ -18,7 +18,7 @@ export const TeamsBackground = styled.div`
    background-position: center;
    background-size: 1200px;
    background-color: ${({ theme }) => theme.variable.mainLight2};
-   background-image: url(./img/bg1.png);
+   background-image: url(https://raw.githubusercontent.com/MaryAnzh/final-task-assets/main/img/bg1.png);
    
    @media (max-width: 1200px) {
     background-size: 100%;
@@ -26,7 +26,7 @@ export const TeamsBackground = styled.div`
 
    @media (max-width: 490px) {
     height: calc(100vw * ${k});
-    background-image: url(./img/bg.png);
+    background-image: url(https://raw.githubusercontent.com/MaryAnzh/final-task-assets/main/img/bg.png);
     background-size: 100%;
    }
 `;
