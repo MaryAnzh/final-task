6b4967c5489184479ea1495ace4c3dd0362e9ed6
Nikita <nikitosminsk100@gmail.com>
2023-02-25 19:44:50 +0300

    feat: add also links to repos of Dima and Nikita

--- src/components/simple/emailLetter/EmailLetter.tsx
+++ src/components/simple/emailLetter/EmailLetter.tsx
@@ -1,7 +1,7 @@
 export const EmailLetter = (): JSX.Element => (
     <article style={{borderRadius: 4, overflow: 'hidden', display: 'flex', flexDirection: 'column', width: '100%', boxShadow: '0 0 10px rgba(0, 0, 0, .2)', maxWidth: '400px'}}>
         <header style={{width: '100%', background: '#314AD7', padding: '20px', color: 'white', fontSize: '25px'}}>
-            <h1 style={{color: 'white'}}>Thank you for your email</h1>
+            <h1 style={{color: 'white'}}>Thank you for subscription</h1>
         </header>
         <main style={{width: '100%', background: 'white', padding: '30px'}}>
             <img src="https://raw.githubusercontent.com/MaryAnzh/final-task-assets/main/img/bg1.png" width="100%"/>
@@ -9,8 +9,9 @@ export const EmailLetter = (): JSX.Element => (
 
             <hr style={{marginBottom: 20, marginTop: 20}}/>
             <span style={{fontSize: 20}}>What about some useful links ?</span>
-            <p style={{fontSize: 15, marginTop: 20, fontStyle: 'italic'}}>Check out team lead's GitHub repositories: </p>
-            <a href="https://github.com/MaryAnzh" style={{borderRadius: 3, background: '#314AD7', display: 'flex', marginTop: 10, padding: 10, color: 'white', width: '100%'}}>Explore</a>
+            <a href="https://github.com/MaryAnzh" target="_blank" style={{borderRadius: 3, background: '#314AD7', display: 'flex', marginTop: 10, padding: 10, color: 'white', width: '100%'}}>Mary's GitHub repository</a>
+            <a href="https://github.com/DmitryBaranovAndreevich" target="_blank" style={{borderRadius: 3, background: '#314AD7', display: 'flex', marginTop: 10, padding: 10, color: 'white', width: '100%'}}>Dmitry's GitHub repository</a>
+            <a href="https://github.com/user-of-github" target="_blank" style={{borderRadius: 3, background: '#314AD7', display: 'flex', marginTop: 10, padding: 10, color: 'white', width: '100%'}}>Nikita's GitHub repository</a>
         </main>
     </article>
 )
