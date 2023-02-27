994c29cc0701ee7742c624c49ccc051f6da96542
MaryAnzh <maryang@mail.ru>
2023-02-27 23:29:56 +0300

    feat: add backend info modal window

---
+++ src/components/containers/about-backend/index.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/router';
+
+import { en } from '../../../data/locales/en';
+import { ru } from '../../../data/locales/ru';
+import { StorageService } from "@/utils/localStorage";
+import { AboutBackEnd, ModalAboutTask, ModalAboutTaskCross, ModalAboutTaskText, WarningWrap } from "./styled";
+
+type AboutBackEndMessageProps = {
+    click: () => void,
+}
+
+export const AboutBackEndMessage = (props: AboutBackEndMessageProps) => {
+    const router = useRouter();
+    const t = router.locale === 'en' ? en : ru;
+
+    return (
+        <AboutBackEnd>
+            <ModalAboutTask>
+                <ModalAboutTaskCross onClick={props.click}></ModalAboutTaskCross>
+                <ModalAboutTaskText>{t.ALERT_1}</ModalAboutTaskText>
+                <ModalAboutTaskText>{t.ALERT_2}</ModalAboutTaskText>
+                <ModalAboutTaskText>{t.ALERT_3}</ModalAboutTaskText>
+                <ModalAboutTaskText>{t.ALERT_4}</ModalAboutTaskText>
+                <ModalAboutTaskText>final-task.ru</ModalAboutTaskText>
+                <ModalAboutTaskText>{t.ALERT_5}</ModalAboutTaskText>
+                <ModalAboutTaskText>https://github.com/DmitryBaranovAndreevich/final-task-plus</ModalAboutTaskText>
+                <h3>{t.ALERT_6}</h3>
+                <ModalAboutTaskText>{t.ALERT_7}</ModalAboutTaskText>
+                <ModalAboutTaskText>{t.ALERT_8}</ModalAboutTaskText>
+                <ModalAboutTaskText>{t.ALERT_9}</ModalAboutTaskText>
+                <ModalAboutTaskText>{t.ALERT_10}</ModalAboutTaskText>
+                <WarningWrap></WarningWrap>
+            </ModalAboutTask>
+        </AboutBackEnd>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,12 +7,13 @@ import { AnimationBlock } from '@/components/smart/animation-block'
 import { TeamInfoArticle } from '@/components/common/info-article/team-info'
 import { TeamInfoBlock } from '@/components/simple/teem-info-block'
 import { AboutPageWrap, TeamsBackground } from '@/styles/styled'
+import { AboutBackEndMessage } from '@/components/containers/about-backend';
 
 import Feedbacks from '@/components/feedbacks'
 import { TeamAnimationBlock } from '@/components/containers/team-anination-block'
 import { StarSCG } from '@/components/simple/starSVG'
 import { PopUp } from '@/components/containers/pop-up';
-import {EmailLetter} from '@/components/simple/emailLetter/EmailLetter'
+import { EmailLetter } from '@/components/simple/emailLetter/EmailLetter'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function Home() {
   const router = useRouter();
   const t = router.locale === 'en' ? en : ru;
 
+  const [isBackEndInfoShow, setBackEndInfoShow] = useState(false);
   const [isModalOpen, setShowCloseModal] = useState(false);
 
   const showModalOnClick = () => {
@@ -30,13 +32,24 @@ export default function Home() {
     setShowCloseModal((prev) => prev = false);
   }
 
+  const showBackEndInfo = () => {
+    setBackEndInfoShow((prev) => prev = true);
+  }
+
+  const closeBackEndInfo = () => {
+    setBackEndInfoShow((prev) => prev = false);
+  }
+
   return (
     <AboutPageWrap>
+      {isBackEndInfoShow &&
+        <AboutBackEndMessage click={closeBackEndInfo} />
+      }
       {
         isModalOpen &&
-        <PopUp isOpen={isModalOpen} closePopUp={closeModalOnClick}/>
+        <PopUp isOpen={isModalOpen} closePopUp={closeModalOnClick} />
       }
-      <AnimationBlock />
+      <AnimationBlock click={showBackEndInfo}/>
       <TeamsBackground>
         <StarSCG onClick={showModalOnClick} />
       </TeamsBackground>
